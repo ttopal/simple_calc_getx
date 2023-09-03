a61665ffceddc7259ef54a97cdeed3e4421e615e
Taner Topal <tanertopal@gmail.com>
2023-09-03 22:29:15 +0300

    add getx

---
+++ lib/controllers/calculator_controller.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:get/get.dart';
+
+class CalculatorController extends GetxController {
+  var firstNumber  = '10'.obs;
+  var secondNumber = '20'.obs;
+  var mathResult   = '30'.obs;
+  var operation    = '+'.obs;
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,48 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
- 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text('Appbar'),
-      ),
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Calculator',
+      theme: ThemeData.dark(useMaterial3: ).copyWith(scaffoldBackgroundColor: Colors.black),
+      home: CalculatorScreen(),
     );
   }
 }
