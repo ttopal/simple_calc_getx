8b4d096cc1a77704da53a4df6017438443d3a2db
Taner Topal <tanertopal@gmail.com>
2023-09-03 22:30:27 +0300

    Some modification

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:calculadora/widgets/calc_button.dart';
-import 'package:calculadora/widgets/line_separator.dart';
-import 'package:calculadora/widgets/main_result.dart';
-import 'package:calculadora/widgets/sub_result.dart';
+import '../widgets/calc_button.dart';
+import '../widgets/line_separator.dart';
+import '../widgets/main_result.dart';
+import '../widgets/sub_result.dart';
 
 
 class CalculatorScreen extends StatelessWidget {
